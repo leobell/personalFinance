8fc874bdbdbd8bbec81332300e5b66d6e97c897f
leobell <bellleonardo04@gmail.com>
2026-09-08 16:27:38 +0200

    Imposta la configurazione del proxy di fiducia per l'app Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
 const app = express()
+app.set('trust proxy', 1)
 const passport = require('./lib/passport')
 
 //middlewares
